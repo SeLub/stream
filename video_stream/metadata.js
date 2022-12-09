5532a226fe8df7af1029044b7768375254a47044
Sergey Lubimov <selub@pop-os.localdomain>
2022-12-09 23:49:27 +0300

    feat: video stream API

---
+++ video_stream/metadata.js
@@ -0,0 +1,12 @@
+const videosList = [
+    {
+        id: '1', duration: '15 secs', file: 'sample-15s.mp4', name: 'Проезжающий мимо парка поток автотранспорта'
+    },
+    {
+        id: '2', duration: '47 mins 52 secs', file: 'file_example_MP4_640_3MG.mp4', name: 'Sample Video file for Testing'
+    },
+    {
+        id: '3', duration: '47 mins 52 secs', file: 'file_example_MP4_640_3MG.mp4', name: 'Sample Video file for Testing'
+    }
+];
+module.exports = videosList
